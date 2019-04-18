b1f48fc6cf34628f60d50e479bcef9b4dc0bce46
atomasovszky <tomasovszky.almos@gmail.com>
2019-04-19 00:29:26 +0200

    create solution for problem 2

--- scripts/week4.R
+++ scripts/week4.R
@@ -29,3 +29,38 @@ data.table(
     test_value   = vowel.test$y
 ) %>%
     .[pred_model_1 == pred_model_2, confusionMatrix(pred_model_1, test_value)]
+
+
+# problem 2
+library(gbm)
+set.seed(3433)
+library(AppliedPredictiveModeling)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+set.seed(62433)
+
+model_rf <- train(diagnosis ~ ., data  = training, method = "rf")
+model_gbm <- train(diagnosis ~ ., data  = training, method = "gbm")
+model_lda <- train(diagnosis ~ ., data  = training, method = "lda")
+
+confusionMatrix(predict(model_rf, testing), testing$diagnosis)$overall[1]
+confusionMatrix(predict(model_gbm, testing), testing$diagnosis)$overall[1]
+confusionMatrix(predict(model_lda, testing), testing$diagnosis)$overall[1]
+
+predicts <- data.table(
+    id  = 1:nrow(testing),
+    rf  = predict(model_rf, testing),
+    gbm = predict(model_gbm, testing),
+    lda = predict(model_lda, testing),
+    truth = testing$diagnosis
+) %>% melt(id.vars = c("id", "truth"))
+
+predicts %>%
+    .[, .N, .(id, truth, value)] %>%
+    .[, value := as.factor(value)] %>%
+    .[, voted := N == max(N), .(id, truth)] %>%
+    .[voted == TRUE] %>%
+    .[, confusionMatrix(value, truth)]
