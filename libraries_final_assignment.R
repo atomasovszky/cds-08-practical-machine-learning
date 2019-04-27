0433705e772a22d2f1196afa466f821073318aef
atomasovszky <tomasovszky.almos@gmail.com>
2019-04-27 15:09:53 +0200

    initial commit for final assignment

---
+++ libraries_final_assignment.R
@@ -0,0 +1,7 @@
+suppressWarnings(suppressPackageStartupMessages(if(!require("data.table")) {install.packages("data.table");  require("data.table")}))
+suppressWarnings(suppressPackageStartupMessages(if(!require("ggplot2")) {install.packages("ggplot2");  require("ggplot2")}))
+suppressWarnings(suppressPackageStartupMessages(if(!require("glue")) {install.packages("glue");  require("glue")}))
+suppressWarnings(suppressPackageStartupMessages(if(!require("swirl")) {install.packages("swirl");  require("swirl")}))
+suppressWarnings(suppressPackageStartupMessages(if(!require("magrittr")) {install.packages("magrittr");  require("swirl")}))
+
+extrafont::loadfonts()
