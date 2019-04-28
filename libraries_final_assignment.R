cda86aa1a163469bb7447be3c1b58f01d804be0b
atomasovszky <tomasovszky.almos@gmail.com>
2019-04-28 20:55:18 +0200

    train randomForest model

--- libraries_final_assignment.R
+++ libraries_final_assignment.R
@@ -1,7 +1,9 @@
 suppressWarnings(suppressPackageStartupMessages(if(!require("data.table")) {install.packages("data.table");  require("data.table")}))
 suppressWarnings(suppressPackageStartupMessages(if(!require("ggplot2")) {install.packages("ggplot2");  require("ggplot2")}))
+suppressWarnings(suppressPackageStartupMessages(if(!require("caret")) {install.packages("caret");  require("swirl")}))
 suppressWarnings(suppressPackageStartupMessages(if(!require("glue")) {install.packages("glue");  require("glue")}))
 suppressWarnings(suppressPackageStartupMessages(if(!require("magrittr")) {install.packages("magrittr");  require("swirl")}))
 suppressWarnings(suppressPackageStartupMessages(if(!require("purrr")) {install.packages("purrr");  require("swirl")}))
+suppressWarnings(suppressPackageStartupMessages(if(!require("doParallel")) {install.packages("doParallel");  require("swirl")}))
 
 extrafont::loadfonts()
